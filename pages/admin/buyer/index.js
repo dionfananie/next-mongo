77a6504d5b19e2ccadb3c600dd1f3e3a13c27b2f
dion fananie <dionfananie@gmail.com>
2022-07-09 20:37:12 +0700

    add improvement list buyer

--- pages/admin/buyer/index.js
+++ pages/admin/buyer/index.js
@@ -3,6 +3,7 @@ import { loadBuyersAll } from '@lib/fetch-data';
 import { arrayOf } from 'prop-types';
 import { object } from 'prop-types';
 import Title from '@components/Admin/Title';
+import { CheckIcon } from '@heroicons/react/solid';
 
 export default function Buyer({ item }) {
     return (
@@ -15,14 +16,15 @@ export default function Buyer({ item }) {
                             <th scope="col" className="px-6 py-3">
                                 Nama Pemesan
                             </th>
-                            <th scope="col" className="px-6 py-3">
-                                Alamat
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                No Handphone
-                            </th>
+
                             <th scope="col" className="px-6 py-3">
                                 Keterangan
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Kelunasan
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Action
                             </th>
                         </tr>
                     </thead>
@@ -33,12 +35,28 @@ export default function Buyer({ item }) {
                                 <tr className="bg-white border-b" key={_id}>
                                     <th
                                         scope="row"
-                                        className="px-6 py-4 font-medium text-gray-900  whitespace-nowrap">
-                                        {name}
+                                        className="px-6 py-4 font-medium whitespace-nowrap">
+                                        <span className="text-gray-900 text-lg">{name}</span>
+                                        <br />
+                                        {address}
+                                        <br />
+                                        {handphone}
                                     </th>
-                                    <td className="px-6 py-4">{address}</td>
-                                    <td className="px-6 py-4">{handphone}</td>
                                     <td className="px-6 py-4">{desc}</td>
+                                    <td className="px-6 py-4">
+                                        <span className="bg-gray-100 text-gray-800 my-1 text-xs font-semibold px-2 py-1 rounded">
+                                            Belum Lunas
+                                        </span>
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        <button
+                                            className={`text-white focus:ring-4 focus:outline-none py-1 rounded-lg text-sm px-4 text-center bg-purple-600 hover:bg-purple-800 focus:ring-purple-300`}>
+                                            <span className="flex items-center">
+                                                <CheckIcon className="w-6 h-6 mr-2" />
+                                                Tandai Sudah Lunas
+                                            </span>
+                                        </button>
+                                    </td>
                                 </tr>
                             );
                         })}
