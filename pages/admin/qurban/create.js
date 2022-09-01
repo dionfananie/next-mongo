bfc6367d0407ed8da4dcd83bda4c952dc727b2a0
dion fananie <dionfananie@gmail.com>
2022-09-01 19:07:38 +0700

    change button tambah

--- pages/admin/qurban/create.js
+++ pages/admin/qurban/create.js
@@ -153,7 +153,7 @@ const FormBuyer = () => {
                     </div>
                     <input
                         type="submit"
-                        value="Pesan"
+                        value="Tambah Qurban"
                         className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
                     />
                 </form>
