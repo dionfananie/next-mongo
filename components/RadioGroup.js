4d5bf6b063b71b403848c36a4aec493dc1e40b9b
dion fananie <dionfananie@gmail.com>
2023-08-18 22:38:25 +0700

    add fix code

--- components/RadioGroup.js
+++ components/RadioGroup.js
@@ -44,7 +44,7 @@ export default function Example() {
                   ${checked ? 'bg-sky-900 bg-opacity-75 text-white' : 'bg-white'}
                     relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
                                 }>
-                                {({ active, checked }) => (
+                                {({ checked }) => (
                                     <>
                                         <div className="flex w-full items-center justify-between">
                                             <div className="flex items-center">
