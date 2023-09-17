2c011b9716cdb6c08813c54929a7eff696db03e0
dion fananie <dionfananie@gmail.com>
2023-09-17 10:22:06 +0700

    auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,8 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
-    console.log('masuk');
-    console.log(req.url);
-    console.log('nextUrl', req.nextUrl);
     const authCookies = req.cookies.get('auth-login');
-    if (!req.nextUrl.pathname.startsWith('/detail') && !authCookies) {
+    if (!authCookies) {
         return NextResponse.rewrite(new URL('/auth/login', req.url));
     }
 
@@ -13,5 +10,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/']
+    matcher: ['/blog', '/detail']
 };
